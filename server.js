683ccc382d87b16667b91b081771f96c7a12e19e
Henry Munoz <alexcol123@yahoo.com>
2020-01-12 00:02:48 -0500

    User Registration

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const express = require("express");
-const connectDB = require('./config/db')
 
+
+const express = require('express');
+const connetDB = require('./config/db');
 
 const app = express();
 
-// Connect Database 
-connectDB();
+//Connect Database
+connetDB();
 
-// Init Midlleware
-app.use(express.json({extended: false}))
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-
-app.get("/", (rep, res) =>
-  res.json({
-    msg: "welcome to contact keeper API..."
-  })
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
 
-// Define Routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("Server started on port ${PORT)"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
